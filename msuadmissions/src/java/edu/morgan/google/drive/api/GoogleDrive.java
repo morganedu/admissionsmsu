0deed056bbecb910061e5203b7d151d3697fb440
Pablo Henrique <pablo.haoko@gmail.com>
2014-03-27 16:14:19 -0400

    GoogleDrive class updated

--- msuadmissions/src/java/edu/morgan/google/drive/api/GoogleDrive.java
+++ msuadmissions/src/java/edu/morgan/google/drive/api/GoogleDrive.java
@@ -24,33 +24,99 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class GoogleDrive {
 
     private static final String CLIENT_ID = "892186241167-228o9c5afo7fqrnbciabv81eghdj63f5.apps.googleusercontent.com";
     private static final String CLIENT_SECRET = "BVuUu5FU8boxFTgkSMtpJwDK";
     private static String REDIRECT_URI;
+    private static String CODE_VALIDATION;
+    private static String AUTHORIZATION_URI;
 
     private HttpTransport httpTransport;
     private JsonFactory jsonFactory;
     private GoogleAuthorizationCodeFlow flow;
     private Drive service;
+    
+    public GoogleDrive(String redirectUri) {
+        REDIRECT_URI = redirectUri;
+        this.httpTransport = new NetHttpTransport();
+        this.jsonFactory = new JacksonFactory();
 
-    private String codeValidation = null;
-    private String authorizationUrl = null;
-
-    public GoogleDrive() {
-        this.instantiateDependencies();
+        this.flow = new GoogleAuthorizationCodeFlow.Builder(
+                this.httpTransport, this.jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
+                .setAccessType("online")
+                .setApprovalPrompt("auto").build();
+        AUTHORIZATION_URI = this.flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
     }
     
-    public GoogleDrive(String redirectUrl) {
-        REDIRECT_URI = redirectUrl;
-        this.instantiateDependencies();
+    public void setCodeValidation(String code){
+        try {
+            CODE_VALIDATION = code;
+            GoogleTokenResponse response = this.flow.newTokenRequest(CODE_VALIDATION).setRedirectUri(REDIRECT_URI).execute();
+            GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
+            this.service = new Drive.Builder(this.httpTransport, this.jsonFactory, credential).build();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public File CreateFolder(String LastName, String FirstName, String ID) throws IOException{
+        return this.CreateFolder(LastName + " " + FirstName + " " + ID);
+    }
+    
+    public File CreateFolder(String ownerName) throws IOException{
+        File body = new File();
+        body.setTitle(ownerName.trim().toUpperCase());
+        body.setMimeType("application/vnd.google-apps.folder");
+        return this.service.files().insert(body).execute(); 
+    }
+    
+    
+    /*
+     * File: Document or Folder
+     *
+     * FileFrom can be used as a Folder OR File!
+     * FileTo can only be used as Folder!!!
+     *
+     */
+    public File MoveFolders(Object fileFrom, Object fileTo) throws IOException{
+        File file, target;
+        if(fileFrom.getClass().equals("String")){
+            file =  this.service.files().get((String) fileFrom).execute();
+            target = this.service.files().get((String) fileTo).execute();
+        }
+        else{
+            file =  this.service.files().get(((File) fileFrom).getId()).execute();
+            target = this.service.files().get(((File) fileTo).getId()).execute();
+        }
+        
+        ParentReference newParent = new ParentReference();
+        newParent.setSelfLink(target.getSelfLink());
+        newParent.setParentLink(target.getParents().get(0).getSelfLink());
+        newParent.setId(target.getId());
+        newParent.setKind(target.getKind());
+        newParent.setIsRoot(false);
+        
+        ArrayList<ParentReference> parentsList = new ArrayList<>();
+        parentsList.add(newParent);
+        file.setParents(parentsList);
+        
+        return this.service.files().update(file.getId(), file).execute();
+    }
+    
+    public File GetFileByTitle(String title) throws IOException{
+        Drive.Files.List list = this.service.files().list();
+        FileList fileList = list.execute();
+        ArrayList<File> arrayListFiles = (ArrayList) fileList.getItems();
+        for(File file : arrayListFiles)
+            if(file.getTitle().equals(title))
+                return file;
+        return null;
     }
     
     /*
-     * Checking of Files and Folders
-     */
     
     public boolean checkFolderExistByUser(String firstName, String lastName) throws IOException{
         return this.checkFolderExistByUser(firstName + " " + lastName);
@@ -70,9 +136,31 @@ public class GoogleDrive {
         return false;
     }
     
-    /*
-     * Listing of Files and Folders
-     */
+    public String checkFolderExistReturnIdByUser(String user) throws IOException{
+        ArrayList<File> userFiles = this.getListFilesTemplate();
+        
+    }
+    
+    
+    
+    public ArrayList<File> listFilesInsideFolderByUser(String firstName, String lastName){
+        return null;
+    }
+    
+    public ArrayList<File> listFilesInsideFolderByUser(String ownerName) throws IOException{
+        ArrayList<File> userFiles = this.getListFilesTemplate();
+        String folderId;
+        
+        for (File file : userFiles)
+            if(!file.getOwnerNames().contains(ownerName.trim()) && file.getMimeType().equals("application/vnd.google-apps.folder")){
+                folderId = file.getId();
+                break;
+            }
+        
+        userFiles.remove(file);
+        return userFiles;
+        return null;
+    }
     
     public ArrayList<File> listFilesByUser(String firstName, String lastName) throws IOException{
         return this.listFilesByUser(firstName + " " + lastName);
@@ -90,9 +178,7 @@ public class GoogleDrive {
         return this.getListFilesTemplate();
     }
     
-    /*
-     * Getters
-     */
+    
     
     public File getFileById(String fileId) throws IOException{
         for (File file : this.getListFilesTemplate()) {
@@ -111,7 +197,11 @@ public class GoogleDrive {
     }
     
     public File getFolderByName(String firstName, String lastName) throws IOException{
-        String folderName = firstName.trim() + " " + lastName.trim();
+        return this.getFolderByName(firstName + " " + lastName);
+    }
+    
+    public File getFolderByName(String ownerName) throws IOException{
+        String folderName = ownerName.trim().toUpperCase();
         for (File folder : this.getListFilesTemplate()) {
             if(folder.getTitle().equals(folderName) && folder.getMimeType().equals("application/vnd.google-apps.folder"))
                 return folder;
@@ -140,9 +230,7 @@ public class GoogleDrive {
         return "";
     }
     
-    /*
-     * Setters
-     */
+    
     
     public void setAuthorizationUrl(String code) {
         this.authorizationUrl = code;
@@ -152,19 +240,18 @@ public class GoogleDrive {
         this.codeValidation = code;
     }
     
-    /*
-     * Creation of Files and Folders
-     */
+    
     
     public void doEverything(String firstName, String lastName, String pathToFile, String fileTitle, String fileDescription, String fileMimeType) throws IOException{
         File folder, file;
-        if(!this.checkFolderExistByUser(firstName, lastName)) // If Folder doesn't exist
-            folder = this.createFolder(firstName, lastName); // Create Folder
+        String ownerName = firstName + " " + lastName;
+        if(!this.checkFolderExistByUser(ownerName)) // If Folder doesn't exist
+            folder = this.createFolder(ownerName); // Create Folder
         else
-            folder = this.getFolderByName(firstName, lastName); // Otherwise, Get Folder
+            folder = this.getFolderByName(ownerName); // Otherwise, Get Folder
         
         if(!this.checkFileExistByName(fileTitle)) // If File doesn't exist.
-            this.uploadFile(pathToFile, fileTitle, fileDescription, fileMimeType, folder.getId());
+            this.uploadFile(pathToFile, fileTitle, fileDescription, fileMimeType, folder.getId(), ownerName);
         else{ // File exist
             file = this.getFileByTitle(fileTitle);
             if(!file.getParents().get(0).getId().equals(folder.getId())) // File isn't inside the folder
@@ -185,8 +272,12 @@ public class GoogleDrive {
     }
     
     public File createFolder(String firstName, String lastName) throws IOException{
+        return createFolder(firstName + " " + lastName); 
+    }
+    
+    public File createFolder(String ownerName) throws IOException{
         File body = new File();
-        body.setTitle(firstName.toUpperCase() + " " + lastName.toUpperCase());
+        body.setTitle(ownerName.trim().toUpperCase());
         body.setMimeType("application/vnd.google-apps.folder");
         return this.service.files().insert(body).execute(); 
     }
@@ -198,17 +289,14 @@ public class GoogleDrive {
             ).execute();
     }
     
-    public File uploadFile(String pathToFile, String fileTitle, String fileDescription, String fileMimeType, String parentId) throws IOException {
+    public File uploadFile(String pathToFile, String fileTitle, String fileDescription, String fileMimeType, String parentId, String ownerName) throws IOException {
         return this.service.files().insert(
-                    this.prepareFile(fileTitle, fileDescription, fileMimeType, parentId),
+                    this.prepareFile(fileTitle, fileDescription, fileMimeType, parentId, ownerName),
                     this.newFileMediaContentTemplate(pathToFile, fileMimeType)
             ).execute();
     }
     
-    /*
-     * Use with care. Private functions
-     *
-     */
+    
     private ArrayList<File> getListFilesTemplate() throws IOException{
         Drive.Files.List list = this.service.files().list();
         FileList fileList = list.execute();
@@ -222,18 +310,6 @@ public class GoogleDrive {
         return mediaContent;
     }
 
-    private void buildService(String codeValidation) {
-        try {
-            GoogleTokenResponse response = this.flow.newTokenRequest(codeValidation).setRedirectUri(REDIRECT_URI).execute();
-            GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
-            this.service = new Drive.Builder(this.httpTransport, this.jsonFactory, credential).build();
-            //return service;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            //return null;
-        }
-    }
-
     private File prepareFile(String title, String description, String mimeType) {
         // Insert a file
         File body = new File();
@@ -243,48 +319,47 @@ public class GoogleDrive {
         return body;
     }
     
-    private File prepareFile(String title, String description, String mimeType, String parentId) {
+    private File prepareFile(String title, String description, String mimeType, String parentId, String ownerName) {
         // Insert a file
         File body = new File();
         body.setTitle(title);
         body.setDescription(description);
         body.setMimeType(mimeType);
+        List<String> ownerList = new ArrayList<>();
+        ownerList.add(ownerName);
         ParentReference newParent = new ParentReference();
         newParent.setId(parentId);
+        body.setOwnerNames(ownerList);
         body.setParents(Arrays.asList(newParent));
         return body;
     }
     
-    private void instantiateDependencies() {
-        this.httpTransport = new NetHttpTransport();
-        this.jsonFactory = new JacksonFactory();
+    */
 
-        this.flow = new GoogleAuthorizationCodeFlow.Builder(
-                this.httpTransport, this.jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
-                .setAccessType("online")
-                .setApprovalPrompt("auto").build();
-        this.authorizationUrl = this.flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
-    }
-
-    /*
-     * Testing functions.
-     * Remove-it when the project is done.
-     */
-    
     public void test() throws IOException {
         System.out.println("Please open the following URL in your browser then type the authorization code:");
-        System.out.println("  " + this.authorizationUrl);
+        System.out.println("  " + AUTHORIZATION_URI);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        this.codeValidation = br.readLine();
-        this.buildService(this.codeValidation);
-
-        //this.uploadFile("/Users/pablohpsilva/Desktop/document.txt", "My Document Example", "This is an example of description", "text/plain");
-        //this.createFolder("fulano", "cicrano");
-        this.doEverything("fulano", "cicrano", "/Users/pablohpsilva/Desktop/document.txt", "My Document Example", "This is an example of description", "text/plain");
+        CODE_VALIDATION = br.readLine();
+        this.setCodeValidation(CODE_VALIDATION);
+        
+        File a = this.CreateFolder("FulanoA", "Cicrano", "123Asad");
+        File b = this.CreateFolder("FulanoB", "Cicrano", "123Asad");
+        
+        this.MoveFolders(a, b);
+        
+        //this.doEverything("fulano", "cicrano", "/Users/pablohpsilva/Desktop/document.txt", "My Document Example", "This is an example of description", "text/plain");
+        /*
+        for (File file : this.listAllFiles())
+            System.out.println(file.getId() + " - " + file.getOwnerNames().get(0) + " - " + file.getTitle() + " - " + file.getMimeType());
+        /*
+        for (File file : this.listFilesByUser("fulano", "cicrano"))
+            System.out.println(file.getId() + " - " +file.getTitle() + " - " + file.getMimeType());
+        */
     }
 
     public static void main(String args[]) {
-        GoogleDrive drive = new GoogleDrive("http://localhost:8084/xml2googledrive/xml2googledriveindex.jsp");
+        GoogleDrive drive = new GoogleDrive("http://localhost:8084/msuadmissions/XML2GoogleDrive/index.jsp");
         try {
             drive.test();
         } catch (Exception ex) {
