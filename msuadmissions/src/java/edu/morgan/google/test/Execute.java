4f1406109ee79908fd24afa86806094487289296
Pablo Henrique <pablo.haoko@gmail.com>
2014-04-18 15:33:42 -0400

    data

--- msuadmissions/src/java/edu/morgan/google/test/Execute.java
+++ msuadmissions/src/java/edu/morgan/google/test/Execute.java
@@ -4,6 +4,7 @@ package edu.morgan.google.test;
 import edu.morgan.users.IncompleteStudent;
 import edu.morgan.users.PrettyStudentPrint;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -51,9 +52,25 @@ public class Execute {
         }
     }
     
-    public void printArray(ArrayList<PrettyStudentPrint> pspArray){
+    public void printArray(ArrayList<PrettyStudentPrint> pspArray) throws IOException{
+        String content = "";
         for(PrettyStudentPrint psp: pspArray)
-            System.out.println(psp.getStudentInfo() + "\n\tFound: " + psp.getFoundChecklist() + "\n\tNot Found: " +psp.getNotFoundChecklist());
+            content += psp.getStudentInfo() + "\n\tFound: " + psp.getFoundChecklist() + "\n\tNot Found: " +psp.getNotFoundChecklist() + "\n\n";
+        
+        File file = new File("/Users/pablohpsilva/Desktop/newfile.txt");
+        FileOutputStream fop = new FileOutputStream(file);
+
+        // if file doesnt exists, then create it
+        if (!file.exists()) {
+                file.createNewFile();
+        }
+
+        // get the content in bytes
+        byte[] contentInBytes = content.getBytes();
+
+        fop.write(contentInBytes);
+        fop.flush();
+        fop.close();
     }
     
 }
